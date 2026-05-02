eaf5d850269e3922557aacd2343b6e73ea3dd55b
Cristhian Renan <cristhianrcampos2@gmail.com>
2026-05-02 01:32:21 -0300

    Primeiras variáveis

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+//Variáveis do algoritmo:
+char placa [10];
+char tipo_veiculo [12];
+int tempo_permanencia, preco_hora, preco_pagar;
+
+//Entrada de dados:
+printf ("Informe a placa do veiculo: ");
+scanf ("%s", &placa);
+
+
+  
